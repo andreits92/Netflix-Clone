6fbd828f100a127759072522858f2bee7bc93886
andreits92 <67470981+andreits92@users.noreply.github.com>
2021-02-13 19:01:12 +0200

    Deploy to firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4WxZlgHFqsPxI1UwmNH0Reni0068K_Cg",
+  authDomain: "netflix-clone-c07b5.firebaseapp.com",
+  projectId: "netflix-clone-c07b5",
+  storageBucket: "netflix-clone-c07b5.appspot.com",
+  messagingSenderId: "231265126625",
+  appId: "1:231265126625:web:258537924fc00991624fa0",
+  measurementId: "G-T0M9JTMGBH",
+};
